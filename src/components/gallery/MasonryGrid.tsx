8a31faedf79b396f0fd9af56b9a3a061da63677d
nthchild2 <nthchild1@gmail.com>
2025-04-24 19:54:08 -0600

    Update MasonryGrid.tsx

--- src/components/gallery/MasonryGrid.tsx
+++ src/components/gallery/MasonryGrid.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import Masonry from "react-masonry-css";
-import { SmartImage } from "@/once-ui/components";
 import styles from "./Gallery.module.scss";
-import { gallery } from "@/app/resources/content";
 
 export default function MasonryGrid() {
   const breakpointColumnsObj = {
@@ -18,19 +16,6 @@ export default function MasonryGrid() {
       breakpointCols={breakpointColumnsObj}
       className={styles.masonryGrid}
       columnClassName={styles.masonryGridColumn}
-    >
-      {gallery.images.map((image, index) => (
-        <SmartImage
-          priority={index < 10}
-          sizes="(max-width: 560px) 100vw, (max-width: 1024px) 50vw, (max-width: 1440px) 33vw, 25vw"
-          key={index}
-          radius="m"
-          aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "9 / 16"}
-          src={image.src}
-          alt={image.alt}
-          className={styles.gridItem}
-        />
-      ))}
-    </Masonry>
+    ></Masonry>
   );
 }
